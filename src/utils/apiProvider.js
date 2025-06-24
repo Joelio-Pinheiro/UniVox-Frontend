dce1e5d315464fcb69e514d984c7c32de6591435
Joelio-Pinheiro <joeliopinheiro10@gmail.com>
2025-06-24 09:22:38 -0300

    feat: implementation apiProvider

--- src/utils/apiProvider.js
+++ src/utils/apiProvider.js
@@ -1,10 +1,11 @@
 import axios from "axios"
-const BASE_API_URL = "http://localhost:3000/api"
+const BASE_API_URL = "https://univox-backend.onrender.com/"
 
 class ApiProvider {
   constructor() {
     this.instance = axios.create({
       baseURL: BASE_API_URL,
+      withCredentials: true,
     })
   }
 
